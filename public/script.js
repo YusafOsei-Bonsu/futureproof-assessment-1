ea9b1ef2cc005ffbe76d9c6cb63bcfb2e5da5b6b
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-11-18 12:21:15 +0000

    Containing entire code in $(document).ready()

--- public/script.js
+++ public/script.js
@@ -1,19 +1,25 @@
-$("form").submit(event => {
+$('document').ready(function() {
+  $("form").submit(event => {
 
   event.preventDefault();
   
-  axios.post("").then(response => {
+  axios.post("localhost:8080/rand").then(response => {
     
     quote = response.data;
-    
+
     $("#text").css({
       "text-align": "center",
       "min-height": "150px",
-      display: "flex",
+      // Changed display into a string so it's recognised as a css prop
+      "display": "flex",
       "justify-content": "center",
       "align-items": "center"
     });
     
     $("#text").html(quote);
+
+  });
   });
 });
+
+
