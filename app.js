804abeac0eb8f58201c379b0605f7ba071f86f6b
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-11-18 11:59:22 +0000

    Update app.js
    - '/quotes/:index' GET route is condensed to a ternary operator.
    - Changed the '/all' GET route
    - Bolden each quote author by encasing their names in <strong> tags

--- app.js
+++ app.js
@@ -18,21 +18,21 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded());
 
 quotes = [
-  '"Perfect is the enemy of good." <br> –Voltaire',
-  '"I’m still learning." <br> –Michelangelo',
-  '"Life is a journey, not a destination." <br> –Ralph Waldo Emerson',
-  '"Learning is not attained by chance, it must be sought for with ardor and attended to with diligence." <br> ―Abigail Adams',
-  '"Yesterday I was clever, so I changed the world. Today I am wise, so I am changing myself." <br> –Rumi',
-  '"Be curious, not judgmental." <br> –Walt Whitman',
-  '"You don’t have to be great to start, but you have to start to be great." <br> –Zig Ziglar',
-  '"Be stubborn about your goals and flexible about your methods." <br> –Unknown',
-  '"Nothing will work unless you do." <br> –Maya Angelou',
-  '"Never give up on a dream just because of the time it will take to accomplish it. The time will pass anyway." <br> –Earl Nightingale',
-  '"Anyone who stops learning is old, whether at twenty or eighty." <br> —Henry Ford',
-  '"Tell me and I forget. Teach me and I remember. Involve me and I learn." <br> –Benjamin Franklin',
-  '"Change is the end result of all true learning." <br> ―Leo Buscaglia',
-  '"Live as if you were to die tomorrow. Learn as if you were to live forever." <br> ―Mahatma Gandhi',
-  '"A learning curve is essential to growth." <br> –Tammy Bjelland'
+  '"Perfect is the enemy of good." – <strong>Voltaire</strong>',
+  '"I’m still learning." – <strong>Michelangelo</strong>',
+  '"Life is a journey, not a destination." – <strong>Ralph Waldo Emerson</strong>',
+  '"Learning is not attained by chance, it must be sought for with ardor and attended to with diligence." ― <strong>Abigail Adams</strong>',
+  '"Yesterday I was clever, so I changed the world. Today I am wise, so I am changing myself." – <strong>Rumi</strong>',
+  '"Be curious, not judgmental." – <strong>Walt Whitman</strong>',
+  '"You don’t have to be great to start, but you have to start to be great." – <strong>Zig Ziglar</strong>',
+  '"Be stubborn about your goals and flexible about your methods." – <strong>Unknown</strong>',
+  '"Nothing will work unless you do." – <strong>Maya Angelou</strong>',
+  '"Never give up on a dream just because of the time it will take to accomplish it. The time will pass anyway." – <strong>Earl Nightingale</strong>',
+  '"Anyone who stops learning is old, whether at twenty or eighty." — <strong>Henry Ford</strong>',
+  '"Tell me and I forget. Teach me and I remember. Involve me and I learn." – <strong>Benjamin Franklin</strong>',
+  '"Change is the end result of all true learning." ― <strong>Leo Buscaglia</strong>',
+  '"Live as if you were to die tomorrow. Learn as if you were to live forever." ― <strong>Mahatma Gandhi</strong>',
+  '"A learning curve is essential to growth." – <strong>Tammy Bjelland</strong>'
 ];
 
 // ES6 version of its former self
@@ -48,28 +48,24 @@ app.use(express.static("public"));
 app.get("/", (req, res) => res.send("index"));
 
 // write route to get all quotes below this line
-app.get('/all', (req, res) => res.status(202).send(quotes));
+app.get('/all', (req, res) => {
+  let allQuotes = '';
+
+  quotes.forEach(quote => allQuotes += `${quote} <br>`);
+
+  res.status(202).send(allQuotes);
+});
 
 // (insert your code here)
-
 //---------------------------
-
 // write route to get a random quote below this line
 app.get('/randomQuote', (req, res) => res.status(202).send(`${getRandomQuote()}`));
 
 // (insert your code here)
-
 //---------------------------
-
 // make sure route can handle errors if index is out of range
-
-app.get("/quotes/:index", (req, res) => {
-  // To get the actual item from the list
-  let index = req.params.index - 1;
-  
-  // If the index is within range, the quote is presented. Otherwise, a 404 page is presented stating that the quote doesn't exist
-  (req.params.index >= 1 && req.params.index <= 15) ? res.status(202).send(quotes[index]) : res.status(404).send(`Error: enter a number between 1 and 15.`);
-});
+// If the index is within the range, a quote is returned. Otherwise, a 404 webpage is presented, stating that the requested quote doesn't exist.
+app.get("/quotes/:index", (req, res) => (req.params.index >= 1 && req.params.index <= 15) ? res.status(202).send(quotes[req.params.index - 1]) : res.status(404).send(`Error: enter a number between 1 and 15.`));
 
 //---------------------------
 app.listen(3000, () => console.log(`Example app listening on port 3000!`));
